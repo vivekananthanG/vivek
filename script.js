6c9084491b4ac7c9e49f940ac03d70f35ef01f51
Vivekananthan <vivekananthan191@gmail.com>
2021-10-27 16:31:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/*You work as a SDE in ABC Softwares. The company has been struggling to manage their product releases. The releases should happen on 25th of every month. Suggest a Git Workflow Architecture for this requirement.
+Simulate this workflow, by creating a pseudo code files and branches, and upload the same to your GitHub Account.
+As a part of solution, share the link to your GitHub repository
+
+1.dev
+2.testing
+
+1.create 3 files.
+index.html ,style.css and script.js
+
+code and push it to gitub using git*/
